5c74926443735b4c45ab665ff799730da2d4d87f
iNeverSleeeeep <362761534@qq.com>
2026-04-01 23:05:47 +0800

    打开所有的扰动力和扰动力矩，验证6000s仿真下，位置和姿态基本符合预期，六自由度仿真基本没有问题。

--- src/parameters/default_sim_config.m
+++ src/parameters/default_sim_config.m
@@ -13,7 +13,7 @@ simConfig.LG0 = 0.0; % Initial longitude (rad)
 simConfig.omega_E = 7.292115e-5; % Earth's rotation rate (rad/s)
 simConfig.X_i0 = [7000e3; 0; 0]; % Initial position in ECI (m)
 simConfig.V_i0 = [0; 7546; 0]; % Initial velocity in ECI (m/s)
-simConfig.q_b0 = [1; 0; 0; 0];
+simConfig.q_b0 = [cos(pi/8); 0; 0; sin(pi/8)]; % [1; 0; 0; 0];
 simConfig.omega_b0 = [0.0; 0; 0.0]; % Initial angular velocity in body frame (rad/s)
 
 end
